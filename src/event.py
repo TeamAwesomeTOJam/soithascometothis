1b96f9bc5365e40581eb4576a813f087ffb9ff66
Jonathan Doda <jonathan@jdoda.ca>
2015-05-02 19:43:25 -0400

    Events are almost working!

--- src/event.py
+++ src/event.py
@@ -7,10 +7,12 @@ class EventComponent(Component):
     
     def add(self, entity):
         verify_attrs(entity, ['event', ('text', ''), 'x', 'y'])
-        event = game.get_game().resource_manager.get('event', entity.event)
+        entity.register_handler('change_node', self.handle_change_node)
+        
+        entity.event = game.get_game().resource_manager.get('event', entity.event)
         entity.current_node = None
         entity.options = []
-        self.handle_change_node(entity, 0)
+        self.handle_change_node(entity, 1)
     
     def remove(self, entity):
         for option_entity in entity.options:
@@ -29,7 +31,7 @@ class EventComponent(Component):
             
             entity.options = []
             
-            for idx, option in enumerate(new_node.options):
+            for idx, option in enumerate(entity.current_node.options):
                 option_entity = Entity('event-option', option=option, x=entity.x, y=entity.y+200+idx*100, event=entity)
                 game.get_game().entity_manager.add_entity(option_entity)
                 entity.options.append(option_entity)
@@ -41,9 +43,10 @@ class EventOptionComponent(Component):
     
     def add(self, entity):
         verify_attrs(entity, ['option', ('text', entity.option.text)])
+        entity.register_handler('activate', self.handle_activate)
         
     def remove(self, entity):
         pass
     
-    def handle_activate(self):
+    def handle_activate(self, entity):
         entity.event.handle('change_node', entity.option.pass_)
