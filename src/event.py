ac08de17643ce73e73ad33d4080419e9bea3fb67
Jonathan Doda <jonathan@jdoda.ca>
2015-05-03 13:22:49 -0400

    Events are pretty close to complete. Just need to evaluate requirements.

--- src/event.py
+++ src/event.py
@@ -19,13 +19,34 @@ class EventComponent(Component):
             game.get_game().entity_manager.remove_entity(option_entity)
             
     def handle_change_node(self, entity, node_id):
-        for node in entity.event:
+        for node in entity.event.nodes:
             if node.id == node_id:
                 entity.current_node = node
         
         if entity.current_node != None:
-            entity.text = entity.current_node.text
+            # Apply effects
+            if hasattr(entity.current_node, 'effects'):
+                for target, delta in entity.current_node.effects._asdict().iteritems():
+                    target_entity_ident, target_attr = target.split('_')
+                    if target_entity_ident == 'human':
+                        target_entities = {game.get_game().entity_manager.get_by_name(entity.event.location).humans[0]}
+                    elif target_entity_ident == 'camp':
+                        target_entities = {game.get_game().entity_manager.get_by_name('camp')}
+                    elif target_entity_ident == 'all':
+                        target_entities = game.get_game().entity_manager.get_by_tag('human')
+                    
+                    for target_entity in target_entities:
+                        setattr(target_entity, target_attr, getattr(target_entity, target_attr) + delta)
             
+            # Set text
+            location = game.get_game().entity_manager.get_by_name(entity.event.location)
+            if len(location.humans) > 0:
+                human_name = location.humans[0].name
+            else:
+                human_name = ''
+            entity.text = entity.current_node.text.format(human_name=human_name)
+            
+            # Clear old options and add new options
             for option_entity in entity.options:
                 game.get_game().entity_manager.remove_entity(option_entity)
             
@@ -36,8 +57,6 @@ class EventComponent(Component):
                 game.get_game().entity_manager.add_entity(option_entity)
                 entity.options.append(option_entity)
 
-    def handle_draw(self, entity, surface):
-        pass
 
 class EventOptionComponent(Component):
     
@@ -49,4 +68,11 @@ class EventOptionComponent(Component):
         pass
     
     def handle_activate(self, entity):
+        if not hasattr(entity.option, 'pass_'): # This is a terminal option
+            for option_entity in entity.event.options:
+                game.get_game().entity_manager.remove_entity(option_entity)
+            game.get_game().entity_manager.remove_entity(entity.event)
+            return
+        
+        
         entity.event.handle('change_node', entity.option.pass_)
